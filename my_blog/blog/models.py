b3c9fb6e401f1adfa70b0130f12377526a4b3f21
ChoSangYeon <113081584+ChoSangYeon@users.noreply.github.com>
2023-11-01 09:31:05 +0900

    add: 카테고리 추가

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -3,19 +3,16 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    thumb_image = models.ImageField(
-        upload_to='blog/images/%Y/%m/%d/', blank=True)
-    file_upload = models.FileField(
-        upload_to='blog/files/%Y/%m/%d/', blank=True)
+    thumb_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     view_count = models.PositiveIntegerField(default=0)
     tags = models.ManyToManyField('Tag', blank=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -38,6 +35,12 @@ class Comment(models.Model):
         return self.message
     
 class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     def __str__(self):
         return self.name
